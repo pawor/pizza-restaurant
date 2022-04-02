425f00b6b79f59c00c827cebf872d74e5a4abc21
pawor <pawor.emi@gmail.com>
2022-04-03 03:19:08 +0700

    navbar and sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,8 +13,23 @@ const Navbar = () => {
           <div className={styles.text}>0986518879</div>
         </div>
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logos.png" alt="" width="160px" height="69px"/>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+        <Image src="/img/cart.png" alt="" width="30px" height="30px"/>
+        <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   )
 }
