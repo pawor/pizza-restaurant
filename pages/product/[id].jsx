5e455222a3ae2f49e757105ebb0643d71224aca9
pawor <pawor.emi@gmail.com>
2022-04-03 12:34:04 +0700

    design pizzalist and pizzacard

---
+++ pages/product/[id].jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Product.module.css'
+import Image from 'next/image'
+
+const Product = () => {
+  return (
+    <div className={styles.container}>[id]</div>
+  )
+}
+
+export default Product
