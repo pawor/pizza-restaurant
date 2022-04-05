ac61bdfa69c49eecdb54247195d28a9ae81e0bb3
pawor <pawor.emi@gmail.com>
2022-04-05 19:10:38 +0700

    responesive web

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -21,7 +21,7 @@ const Order = ({order}) => {
       <div className={styles.left}>
         <div className={styles.row}>
           <table className={styles.table}>
-            <tr className={styles.tr}>
+            <tr className={styles.trTitle}>
               <th>Order ID</th>
               <th>Customer</th>
               <th>Address</th>
