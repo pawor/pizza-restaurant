1f41935c0829453ade26aa13d61fd536f5ed4019
pawor <pawor.emi@gmail.com>
2022-04-05 12:28:05 +0700

    login

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,16 @@
-const handler = async (req,res) => {
+import cookie from 'cookie'
+
+const handler =  (req,res)  => {
+
+    if( req.method === "POST" ) {
+        const { username, password } = req.body
+
+        if( username === process.env.USERNAME  &&  password === process.env.PASSWORD ) {
+            res.setHeaders( "Set-Cookie", cookie.serialize( "token", process.env.TOKEN, { maxAge: 60*60, sameSite:"strict", path:"/"}))
+            res.status(200).json( "Successfull" )
+        }
+    }
     
 }
+
 export default handler
